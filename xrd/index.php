b3ab8c34cab7bd81cd16deca4c971c3db8aded94
warp <kuno@frob.nl>
2012-07-10 23:00:49 +0200

    Copy some xrd stuff from micro.frob.nl to frob.nl.

--- xrd/index.php
+++ xrd/index.php
@@ -16,6 +16,18 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
     <Subject><?php echo $subject; ?></Subject>
-    <Link rel="http://webfinger.net/rel/profile-page" type="text/html" href="http://320x200.org/profile/"></Link>
-    <Link rel="http://schemas.google.com/g/2010#updates-from" href="http://320x200.org/pixie.atom" type="application/atom+xml"></Link>
+    <Alias>https://micro.frob.nl/</Alias>
+    <Link rel="http://webfinger.net/rel/profile-page" type="text/html" href="https://micro.frob.nl/"></Link>
+    <Link rel="describedby" type="application/rdf+xml" href="https://micro.frob.nl/foaf"></Link>
+    <Link rel="salmon" href="https://micro.frob.nl/main/salmon/user/1"></Link>
+    <Link rel="http://salmon-protocol.org/ns/salmon-replies" href="https://micro.frob.nl/main/salmon/user/1"></Link>
+    <Link rel="http://salmon-protocol.org/ns/salmon-mention" href="https://micro.frob.nl/main/salmon/user/1"></Link>
+    <Link rel="http://schemas.google.com/g/2010#updates-from" href="https://micro.frob.nl/api/statuses/user_timeline/1.atom" type="application/atom+xml">
+    <Link rel="http://ostatus.org/schema/1.0/subscribe" template="https://micro.frob.nl/main/ostatussub?profile={uri}"></Link>
+    <Link rel="http://apinamespace.org/atom" type="application/atomsvc+xml" href="https://micro.frob.nl/api/statusnet/app/service/warp.xml">
+        <Property type="http://apinamespace.org/atom/username">warp</Property>
+    </Link>
+    <Link rel="http://apinamespace.org/twitter" href="https://micro.frob.nl/api/">
+        <Property type="http://apinamespace.org/twitter/username">byld</Property>
+    </Link>
 </XRD>
