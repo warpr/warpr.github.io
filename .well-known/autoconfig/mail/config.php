1051ee1a3a393374499b00d33f2ce010255d4f65
warp <kuno@frob.nl>
2015-01-07 11:20:26 +0100

    mail configuration, .online and pomodoro page.

--- .well-known/autoconfig/mail/config.php
+++ .well-known/autoconfig/mail/config.php
@@ -2,49 +2,39 @@
 
 header ('Content-Type: application/xml');
 
-$mailboxname = str_replace ("@", "_", $_GET["emailaddress"]);
-
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
-
 <clientConfig version="1.1">
-  <emailProvider id="frob.nl">
+  <emailProvider id="MessagingEngine">
     <domain>frob.nl</domain>
     <displayName>frob.nl</displayName>
     <displayShortName>frob.nl</displayShortName>
     <incomingServer type="imap">
-      <hostname>mail.mxes.net</hostname>
+      <hostname>mail.messagingengine.com</hostname>
       <port>993</port>
       <socketType>SSL</socketType>
       <authentication>password-cleartext</authentication>
-      <username><?php echo ("$mailboxname"); ?></username>
+      <username>%EMAILADDRESS%</username>
     </incomingServer>
-    <incomingServer type="imap">
-      <hostname>mail.mxes.net</hostname>
-      <port>143</port>
-      <socketType>STARTTLS</socketType>
+    <incomingServer type="pop3">
+      <hostname>mail.messagingengine.com</hostname>
+      <port>995</port>
+      <socketType>SSL</socketType>
       <authentication>password-cleartext</authentication>
-      <username><?php echo ("$mailboxname"); ?></username>
+      <username>%EMAILADDRESS%</username>
     </incomingServer>
     <outgoingServer type="smtp">
-      <hostname>smtp.mxes.net</hostname>
+      <hostname>mail.messagingengine.com</hostname>
       <port>465</port>
       <socketType>SSL</socketType>
       <authentication>password-cleartext</authentication>
-      <username><?php echo ("$mailboxname"); ?></username>
+      <username>%EMAILADDRESS%</username>
     </outgoingServer>
-    <outgoingServer type="smtp">
-      <hostname>smtp.mxes.net</hostname>
-      <port>587</port>
-      <socketType>STARTTLS</socketType>
-      <authentication>password-cleartext</authentication>
-      <username><?php echo ("$mailboxname"); ?></username>
-    </outgoingServer>
-    <documentation url="http://www.tuffmail.com/client-config.php">
-      <descr lang="en">Tuffmail client configuration page</descr>
-    </documentation>
-    <documentation url="http://www.tuffmail.com/ports.php">
-      <descr lang="en">Tuffmail server settings page</descr>
-    </documentation>
+    <instruction url="https://www.fastmail.fm/help/technical/servernamesandports.html">
+      <descr lang="en">Server Names and Ports</descr>
+    </instruction>
+    <instruction url="https://www.fastmail.fm/help/clients/thunderbird.html">
+      <descr lang="en">Using FastMail with Mozilla Thunderbird</descr>
+    </instruction>
   </emailProvider>
 </clientConfig>
