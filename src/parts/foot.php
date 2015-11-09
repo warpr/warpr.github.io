164b0fb8415eac8acd5e9c030900cb046a55739e
kuno <kuno@frob.nl>
2015-11-08 22:18:43 -0500

    Remove bower

--- src/parts/foot.php
+++ src/parts/foot.php
@@ -1,14 +1,14 @@
 <?php if (getenv("FROB") == "static"): ?>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
-  <script src="/bower_components/momentjs/moment.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script src="/node_modules/moment/moment.js"></script>
   <script src="/node_modules/moment-timezone/moment-timezone.js"></script>
 <?php else: ?>
-  <script src="../bower_components/jquery/jquery.min.js"></script>
-  <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-  <script src="../bower_components/less/dist/less-1.5.1.js"></script>
-  <script src="../bower_components/momentjs/moment.js"></script>
-  <script src="../node_modules/moment-timezone/moment-timezone.js"></script>
+  <script src="/node_modules/jquery/dist/jquery.js"></script>
+  <script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+  <script src="/node_modules/less/dist/less.js"></script>
+  <script src="/node_modules/moment/moment.js"></script>
+  <script src="/node_modules/moment-timezone/moment-timezone.js"></script>
 <?php endif; ?>
 <script src="/site.js"></script>
 
--- src/parts/head.php
+++ src/parts/head.php
@@ -8,13 +8,13 @@
 
 
 <?php if (getenv("FROB") == "static"): ?>
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.2/css/font-awesome.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.css">
   <link rel="stylesheet" href="/style.css" />
 <?php else: ?>
   <meta http-equiv="refresh" content="20" />
-  <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/node_modules/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet/less" href="style.less" />
 <?php endif; ?>
 
